e4824cc9505af9c1cedd8620dd6ea6599319a422
Tilak Raj <trkhanna10018@gmail.com>
2026-09-16 12:55:20 +0530

    Create joa-question-bank.js

---
+++ joa-question-bank.js
@@ -0,0 +1,127 @@
+const questions = [
+
+  {
+    id: 1,
+    en: "",
+    hi: "",
+    A: "",
+    B: "",
+    C: "",
+    D: "",
+    ans: "",
+    explanation: ""
+  },
+
+  {
+    id: 2,
+    en: "",
+    hi: "",
+    A: "",
+    B: "",
+    C: "",
+    D: "",
+    ans: "",
+    explanation: ""
+  },
+
+  {
+    id: 3,
+    en: "",
+    hi: "",
+    A: "",
+    B: "",
+    C: "",
+    D: "",
+    ans: "",
+    explanation: ""
+  },
+
+  {
+    id: 4,
+    en: "",
+    hi: "",
+    A: "",
+    B: "",
+    C: "",
+    D: "",
+    ans: "",
+    explanation: ""
+  },
+
+  {
+    id: 5,
+    en: "",
+    hi: "",
+    A: "",
+    B: "",
+    C: "",
+    D: "",
+    ans: "",
+    explanation: ""
+  },
+
+  {
+    id: 6,
+    en: "",
+    hi: "",
+    A: "",
+    B: "",
+    C: "",
+    D: "",
+    ans: "",
+    explanation: ""
+  },
+
+  {
+    id: 7,
+    en: "",
+    hi: "",
+    A: "",
+    B: "",
+    C: "",
+    D: "",
+    ans: "",
+    explanation: ""
+  },
+
+  {
+    id: 8,
+    en: "",
+    hi: "",
+    A: "",
+    B: "",
+    C: "",
+    D: "",
+    ans: "",
+    explanation: ""
+  },
+
+  {
+    id: 9,
+    en: "",
+    hi: "",
+    A: "",
+    B: "",
+    C: "",
+    D: "",
+    ans: "",
+    explanation: ""
+  },
+
+  {
+    id: 10,
+    en: "",
+    hi: "",
+    A: "",
+    B: "",
+    C: "",
+    D: "",
+    ans: "",
+    explanation: ""
+  },
+
+  // Continue the same format up to Question 100
+
+];
+
+export { questions };
